7118cda91491629a638c8281aa35be8dc5eb859d
RoksanaKop <roksana.kopiec91@gmail.com>
2023-02-25 18:57:59 +0100

    Zadanie praktyczne - Artykuł. Proszę o sprawdzenie

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+ console.log("Roksana Kopiec");
+ document.getElementById("name").innerHTML = "Roksana";
+ document.getElementById("lastname").innerHTML = "Kopiec";
+
+ document.write('Roksana Kopiec')
